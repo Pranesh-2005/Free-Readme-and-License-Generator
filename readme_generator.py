f34677ae3c91e6aa22fd8ccafaf9f6eec7f63b6f
Pranesh <praneshmadhan646@gmail.com>
2025-11-01 19:44:49 +0530

    added final Readme generator file

---
+++ readme_generator.py
@@ -0,0 +1,6 @@
+def generate_readme(repo_info, content, license_type=None):
+    with open("readme.md", "w", encoding="utf-8") as f:
+        f.write(content)
+        if license_type:
+            f.write(f"\n\n## License\nThis project is licensed under the **{license_type}** License.")
+        f.write(f"\n\n---\n🔗 GitHub Repo: {repo_info['url']}")
